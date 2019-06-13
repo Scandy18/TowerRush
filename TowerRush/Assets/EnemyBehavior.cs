947146510578b06187df0d9a9b81947605171f70
Scandy18 <406900308@qq.com>
2019-06-13 11:36:42 +0800

    Init Demo
    白模

---
+++ TowerRush/Assets/EnemyBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+
+    public Transform transform;
+
+    public bool is_dead;
+    public int hp;
+
+    void Start()
+    {
+        is_dead = false;
+        transform = gameObject.GetComponent<Transform>();
+        hp = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0 && !is_dead)
+        {
+            Debug.Log("Enemy dies.");
+            is_dead = true;
+        }
+    }
+
+    public void beAttacked(int atk)
+    {
+        hp -= atk;
+        Debug.Log("Enemy be attacked " + atk + "hp, now " + hp +"hp.");
+    }
+}
---
+++ TowerRush/Assets/TowerBehavior.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBehavior : MonoBehaviour
+{
+
+    private enum Tower_ty { None, Arch, Stone, Ice, Magic }
+
+    //区域属性
+    private bool is_selected = true;
+    private bool is_built = false;
+
+    //塔属性
+    private Transform transform;
+    private Tower_ty ty;
+    private int hp;
+    private int attack;
+    private int atk_speed;
+    private float atk_range;
+
+    //塔模型
+    public GameObject go_arch;
+    public GameObject go_stone;
+    public GameObject go_ice;
+    public GameObject go_magic;
+
+    //敌人列表
+    public List<EnemyBehavior> Enemies;
+
+    void Start()
+    {
+        transform = gameObject.GetComponent<Transform>();
+        Tower_Destroy();
+        hp = 100;
+        attack = 0;
+        atk_speed = 0;
+        atk_range = 0;
+        ty = Tower_ty.None;
+    }
+
+    void Update()
+    {
+        //选中未建造区域可以建造塔
+        if(is_selected && !is_built)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                ty = Tower_ty.Arch;
+                go_arch.SetActive(true);
+                attack = 10;
+                atk_speed = 20;
+                atk_range = 5;
+                Debug.Log("Set to Arch Tower.");
+                is_built = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                ty = Tower_ty.Stone;
+                go_stone.SetActive(true);
+                attack = 20;
+                atk_speed = 10;
+                atk_range = 5;
+                Debug.Log("Set to Stone Tower.");
+                is_built = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+            {
+                ty = Tower_ty.Ice;
+                go_ice.SetActive(true);
+                attack = 5;
+                atk_speed = 10;
+                atk_range = 5;
+                Debug.Log("Set to Ice Tower.");
+                is_built = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
+            {
+                ty = Tower_ty.Magic;
+                go_magic.SetActive(true);
+                attack = 30;
+                atk_speed = 30;
+                atk_range = 5;
+                Debug.Log("Set to Magic Tower.");
+                is_built = true;
+            }
+        }
+        //选中已建造区域可以摧毁塔
+        if(is_selected && is_built)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+            {
+                ty = Tower_ty.None;
+                Tower_Destroy();
+                attack = 0;
+                atk_speed = 0;
+                atk_range = 5;
+                Debug.Log("Destroy the Tower.");
+                is_built = false;
+            }
+        }
+        //攻击逻辑
+        foreach (var enemy in Enemies)
+        {
+            float dist = Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(enemy.transform.position.x, enemy.transform.position.y));
+            //在攻击范围内
+            if (dist < 100 && attack != 0 && enemy.hp > 0)
+            {
+                enemy.beAttacked(attack);
+            }
+        }
+    }
+
+    private void Tower_Destroy()
+    {
+        ty = Tower_ty.None;
+        go_arch.SetActive(false);
+        go_stone.SetActive(false);
+        go_ice.SetActive(false);
+        go_magic.SetActive(false);
+    }
+}
