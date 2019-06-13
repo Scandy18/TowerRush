e3ad896cd8460532bf0303e0f8b6407068bc7ad7
Scandy18 <406900308@qq.com>
2019-06-13 15:00:45 +0800

    以时间为单位攻击

--- TowerRush/Assets/TowerBehavior.cs
+++ TowerRush/Assets/TowerBehavior.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class TowerBehavior : MonoBehaviour
 {
 
     private enum Tower_ty { None, Arch, Stone, Ice, Magic }
-
+    private float timer;
     //区域属性
     private bool is_selected = true;
     private bool is_built = false;
@@ -16,7 +17,7 @@ public class TowerBehavior : MonoBehaviour
     private Tower_ty ty;
     private int hp;
     private int attack;
-    private int atk_speed;
+    private float atk_time;
     private float atk_range;
 
     //塔模型
@@ -31,12 +32,15 @@ public class TowerBehavior : MonoBehaviour
     void Start()
     {
         transform = gameObject.GetComponent<Transform>();
+        timer = 0;
+
         Tower_Destroy();
         hp = 100;
         attack = 0;
-        atk_speed = 0;
         atk_range = 0;
         ty = Tower_ty.None;
+        atk_time = Mathf.Infinity;
+        
     }
 
     void Update()
@@ -49,7 +53,7 @@ public class TowerBehavior : MonoBehaviour
                 ty = Tower_ty.Arch;
                 go_arch.SetActive(true);
                 attack = 10;
-                atk_speed = 20;
+                atk_time = 0.5f;
                 atk_range = 5;
                 Debug.Log("Set to Arch Tower.");
                 is_built = true;
@@ -59,7 +63,7 @@ public class TowerBehavior : MonoBehaviour
                 ty = Tower_ty.Stone;
                 go_stone.SetActive(true);
                 attack = 20;
-                atk_speed = 10;
+                atk_time = 1f;
                 atk_range = 5;
                 Debug.Log("Set to Stone Tower.");
                 is_built = true;
@@ -69,7 +73,7 @@ public class TowerBehavior : MonoBehaviour
                 ty = Tower_ty.Ice;
                 go_ice.SetActive(true);
                 attack = 5;
-                atk_speed = 10;
+                atk_time = 1f;
                 atk_range = 5;
                 Debug.Log("Set to Ice Tower.");
                 is_built = true;
@@ -79,7 +83,7 @@ public class TowerBehavior : MonoBehaviour
                 ty = Tower_ty.Magic;
                 go_magic.SetActive(true);
                 attack = 30;
-                atk_speed = 30;
+                atk_time = 0.4f;
                 atk_range = 5;
                 Debug.Log("Set to Magic Tower.");
                 is_built = true;
@@ -93,20 +97,22 @@ public class TowerBehavior : MonoBehaviour
                 ty = Tower_ty.None;
                 Tower_Destroy();
                 attack = 0;
-                atk_speed = 0;
+                atk_time = Mathf.Infinity;
                 atk_range = 5;
                 Debug.Log("Destroy the Tower.");
                 is_built = false;
             }
         }
         //攻击逻辑
+        timer += Time.deltaTime;
         foreach (var enemy in Enemies)
         {
             float dist = Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(enemy.transform.position.x, enemy.transform.position.y));
-            //在攻击范围内
-            if (dist < 100 && attack != 0 && enemy.hp > 0)
+            //条件：在攻击范围内，有攻击力，敌人没死，攻击CD好了
+            if (dist < 100 && attack != 0 && enemy.hp > 0 && timer >= atk_time)
             {
                 enemy.beAttacked(attack);
+                timer = 0;
             }
         }
     }
