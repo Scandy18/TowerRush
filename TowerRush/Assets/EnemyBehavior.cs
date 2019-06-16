5f5e40ec1459114ab152c9fd507f318b8f9c28a7
Scandy18 <406900308@qq.com>
2019-06-16 14:02:49 +0800

    塔建造函数整合

--- TowerRush/Assets/EnemyBehavior.cs
+++ TowerRush/Assets/EnemyBehavior.cs
@@ -7,11 +7,19 @@ public class EnemyBehavior : MonoBehaviour
 
     public Transform transform;
 
+    private int route_count;
+    private List<Vector3> route;
+    private float move_speed = 0.5f;
     public bool is_dead;
     public int hp;
 
     void Start()
     {
+        route_count = 0;
+        route.Add(new Vector3(-1, 0.5f, 1));
+        route.Add(new Vector3(4, 0.5f, 2));
+        route.Add(new Vector3(9, 0.5f, 1));
+
         is_dead = false;
         transform = gameObject.GetComponent<Transform>();
         hp = 100;
@@ -24,6 +32,14 @@ public class EnemyBehavior : MonoBehaviour
         {
             Debug.Log("Enemy dies.");
             is_dead = true;
+        }
+        else //活着
+        {
+            if(route_count < route.Count)
+            {
+                (route[route_count + 1] - route[route_count]).normalized * move_speed
+
+            }
         }
     }
 
--- TowerRush/Assets/TowerBehavior.cs
+++ TowerRush/Assets/TowerBehavior.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class TowerBehavior : MonoBehaviour
 {
 
-    private enum Tower_ty { None, Arch, Stone, Ice, Magic }
+    public enum Tower_ty { None, Arch, Stone, Ice, Magic }
     private float timer;
     //区域属性
     private bool is_selected = true;
@@ -34,7 +34,7 @@ public class TowerBehavior : MonoBehaviour
         transform = gameObject.GetComponent<Transform>();
         timer = 0;
 
-        Tower_Destroy();
+        Set_Tower(Tower_ty.None);
         hp = 100;
         attack = 0;
         atk_range = 0;
@@ -51,42 +51,26 @@ public class TowerBehavior : MonoBehaviour
             if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
             {
                 ty = Tower_ty.Arch;
-                go_arch.SetActive(true);
-                attack = 10;
-                atk_time = 0.5f;
-                atk_range = 5;
+                Set_Tower(Tower_ty.Arch);
                 Debug.Log("Set to Arch Tower.");
-                is_built = true;
             }
             if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
             {
                 ty = Tower_ty.Stone;
-                go_stone.SetActive(true);
-                attack = 20;
-                atk_time = 1f;
-                atk_range = 5;
+                Set_Tower(Tower_ty.Stone);
                 Debug.Log("Set to Stone Tower.");
-                is_built = true;
             }
             if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
             {
                 ty = Tower_ty.Ice;
-                go_ice.SetActive(true);
-                attack = 5;
-                atk_time = 1f;
-                atk_range = 5;
+                Set_Tower(Tower_ty.Ice);
                 Debug.Log("Set to Ice Tower.");
-                is_built = true;
             }
             if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4))
             {
                 ty = Tower_ty.Magic;
-                go_magic.SetActive(true);
-                attack = 30;
-                atk_time = 0.4f;
-                atk_range = 5;
+                Set_Tower(Tower_ty.Magic);
                 Debug.Log("Set to Magic Tower.");
-                is_built = true;
             }
         }
         //选中已建造区域可以摧毁塔
@@ -95,12 +79,8 @@ public class TowerBehavior : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
             {
                 ty = Tower_ty.None;
-                Tower_Destroy();
-                attack = 0;
-                atk_time = Mathf.Infinity;
-                atk_range = 5;
+                Set_Tower(Tower_ty.None);
                 Debug.Log("Destroy the Tower.");
-                is_built = false;
             }
         }
         //攻击逻辑
@@ -116,13 +96,50 @@ public class TowerBehavior : MonoBehaviour
             }
         }
     }
+    
 
-    private void Tower_Destroy()
+    public void Set_Tower(Tower_ty ty)
     {
-        ty = Tower_ty.None;
-        go_arch.SetActive(false);
-        go_stone.SetActive(false);
-        go_ice.SetActive(false);
-        go_magic.SetActive(false);
+        switch(ty)
+        {
+            case Tower_ty.None:
+                attack = 0;
+                atk_time = Mathf.Infinity;
+                atk_range = 5;
+                go_arch.SetActive(false);
+                go_stone.SetActive(false);
+                go_ice.SetActive(false);
+                go_magic.SetActive(false);
+                is_built = false;
+                break;
+            case Tower_ty.Arch:
+                go_arch.SetActive(true);
+                attack = 10;
+                atk_time = 0.5f;
+                atk_range = 5;
+                is_built = true;
+                break;
+            case Tower_ty.Stone:
+                go_stone.SetActive(true);
+                attack = 20;
+                atk_time = 1f;
+                atk_range = 5;
+                is_built = true;
+                break;
+            case Tower_ty.Ice:
+                go_ice.SetActive(true);
+                attack = 5;
+                atk_time = 1f;
+                atk_range = 5;
+                is_built = true;
+                break;
+            case Tower_ty.Magic:
+                go_magic.SetActive(true);
+                attack = 30;
+                atk_time = 0.4f;
+                atk_range = 5;
+                is_built = true;
+                break;
+        }
     }
 }
