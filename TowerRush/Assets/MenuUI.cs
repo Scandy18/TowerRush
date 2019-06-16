feaa037a5c377a95d83e553e341602af1588de64
Scandy18 <406900308@qq.com>
2019-06-16 16:15:24 +0800

    重整

---
+++ TowerRush/Assets/MenuUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour
+{
+
+    public GameObject
+        Menu,
+        LevelSelecter;
+
+    public void SelectLevel()
+    {
+        LevelSelecter.SetActive(true);
+        Menu.SetActive(false);
+    }
+
+    void Start()
+    {
+        LevelSelecter.SetActive(false);
+    }
+
+}
