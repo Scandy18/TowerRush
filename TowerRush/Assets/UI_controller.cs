a1b1fae2296709c47f99638faff290163c72e28a
shouyuantianxia <39817475+shouyuantianxia@users.noreply.github.com>
2019-06-17 20:38:08 +0800

    UI update

---
+++ TowerRush/Assets/UI_controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class UI_controller : MonoBehaviour
+{
+    public GameObject settings;
+    public void button_start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void button_pause()
+    {
+        Time.timeScale = 0;
+    }
+    public void button_accelerate()
+    {
+        Time.timeScale = 0.5F;
+    }
+    public void button_settings()
+    {
+        if (settings.activeSelf==false)
+        {
+            Time.timeScale = 0;
+            settings.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            settings.SetActive(false);
+        }
+
+    }
+    public void return_main_menu()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+}
