69de04c8b7178247b87621146f9eccd8cd57af4e
Scandy18 <406900308@qq.com>
2019-06-26 15:29:52 +0800

    6.26.15
    敌人延迟出生（用于分批）但无法不显示
    敌人到达终点消失无法被攻击
    敌人路线由外部设定

--- TowerRush/Assets/EnemyBehavior.cs
+++ TowerRush/Assets/EnemyBehavior.cs
@@ -6,50 +6,44 @@ public class EnemyBehavior : MonoBehaviour
 {
     //UI
     GameObject UI;
-
+    //组件
     public Transform transform;
+    public MeshRenderer meshRenderer;
+    //出生
     public float SpawnTime;
     private float Timer;
-
+    private bool can_be_atk;
+    //行进
     private int route_count;
-    private List<Vector3> route;
+    public List<Vector3> route;
     private float move_speed;
-
+    //属性
     public GameObject hp_bar;
     public Transform hp_bar_tran;
-    public bool is_dead;
+    private bool is_dead;
     public float def;
     public int hp;
     public int reward = 10;//奖励money
     public int damage = 10;//对博士的伤害
     //用于减速
     private float slow_timer = 0;
-    private bool is_slowed = false;
+    private bool is_slowed;
 
     
 
     void Start()
     {
         route_count = 0;
-        //设定路线
-        route = new List<Vector3>();
-        route.Add(new Vector3(-3, 0.5f, 2));
-        route.Add(new Vector3(-3, 0.5f, 1));
-        route.Add(new Vector3(-1, 0.5f, 1));
-        route.Add(new Vector3(-1, 0.5f, 0));
-        route.Add(new Vector3(2, 0.5f, 0));
-        route.Add(new Vector3(2, 0.5f, -2));
-        route.Add(new Vector3(1, 0.5f, -2));
-        route.Add(new Vector3(1, 0.5f, -3));
-        route.Add(new Vector3(-3, 0.5f, -3));
-
+        is_slowed = false;
         is_dead = false;
         def = 0.5f;
         hp = 500;
         move_speed = 0.5f;
-
         hp_bar_tran = hp_bar.GetComponent<Transform>();
         transform = gameObject.GetComponent<Transform>();
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        meshRenderer.enabled = false;
+        can_be_atk = false;
         transform.position = route[0];
     }
 
@@ -83,20 +77,35 @@ public class EnemyBehavior : MonoBehaviour
             UI = GameObject.Find("BasicAttribute");
             UI.GetComponent<Attribute_UI_controller>().enemy_death(reward);
         }
+        //到达终点
+        if(route_count >= route.Count - 1)
+        {
+            Debug.Log("Enemy arrives.");
+            gameObject.SetActive(false);
+        }
         //匀速移动
-        if (!is_dead && route_count < route.Count - 1)
+        if (!is_dead && route_count < route.Count - 1 && meshRenderer.enabled)
         {
             transform.position += (route[route_count + 1] - route[route_count]).normalized * move_speed * Time.deltaTime;
             if (Vector3.Distance(transform.position, route[route_count + 1]) < 0.02f)
                 route_count++;
+        }
+        //敌人出生了
+        if (Timer >= SpawnTime)
+        {
+            meshRenderer.enabled = true;
+            can_be_atk = true;
         }
     }
 
     //被攻击接口
     public void BeAttacked(int atk)
     {
-        hp -= atk;
-        Debug.Log("Enemy be attacked " + atk + "hp, now " + hp +"hp.");
+        if(can_be_atk)
+        {
+            hp -= atk;
+            Debug.Log("Enemy be attacked " + atk + "hp, now " + hp + "hp.");
+        }
     }
 
     public void BeSlowed()
